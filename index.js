01716860f6be0ca32779ac0958eff9b0fe299b77
“Hiroki66” <hiroki19981006@gmail.com>
2021-04-21 22:52:19 +0900

    HERO

--- index.js
+++ index.js
@@ -1,14 +1,53 @@
-const user = ['田中三郎','佐藤一郎', '山田三郎', '伊藤花子'];
-user.forEach(name => {
-    if(name === '山田三郎') {
-        console.log('山田三郎さんは存在します');
-    } else if(name === '山田花子') {
-        console.log('山田花子さんは存在します');
-    } else if(name === '田中三郎') {
-        console.log('田中三郎さんは存在します');
-    } else if (name === '佐藤一郎') {
-        console.log('test');
-    } else {
-        console.log('該当するユーザーは存在しません');
+class Monster {
+    constructor(name, hp) {
+        this.adana = name;
+        this.life = hp;
     }
-})
+    superAttack() {
+        return this.adana + 'のウルトラ攻撃！';
+    }
+    getSuperHp() {
+        return this.life + 'です！';
+    }
+}
+
+class Character extends Monster {
+    constructor(name, hp) {
+        super(name, hp);
+        this.name = name;
+        this.hp = hp;
+    }
+    attack() {
+        return this.name + 'の攻撃！';
+    }
+    getHp() {
+        console.log(super.superAttack());
+        return this.hp + 'です！';
+    }
+}
+
+
+class Hero extends Character {
+    constructor(name, hp, exp) {
+        super(name, hp);
+        this.exp = exp;
+    }
+    attack() {
+        console.log(this.name + 'の攻撃！！！！');
+        return super.getHp();
+    }
+    getName() {
+        return this.hp;
+    }
+}
+  
+class Enemy extends Character {
+
+    dropItem() {}
+    
+    getPoint() {}
+    
+}
+
+const hero = new Hero('主人公', 100, 10);
+console.log(hero.superAttack());
